1233876ee5556695fd6039d02bfb26eb1a3cc9f2
Brian Ward <brianmward99@gmail.com>
2025-09-30 12:41:34 -0400

    Add basic build CI

--- src/stan.rs
+++ src/stan.rs
@@ -18,7 +18,7 @@ impl StanExtension {
         };
 
         if let Some(path) = &self.cached_binary_path {
-            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+            if fs::metadata(path).is_ok_and(|stat| stat.is_file()) {
                 return Ok(path.clone());
             }
         }
@@ -64,7 +64,7 @@ impl StanExtension {
             zed::Os::Windows => format!("{version_dir}/stan-language-server.exe"),
         };
 
-        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+        if !fs::metadata(&binary_path).is_ok_and(|stat| stat.is_file()) {
             zed::set_language_server_installation_status(
                 language_server_id,
                 &zed::LanguageServerInstallationStatus::Downloading,
